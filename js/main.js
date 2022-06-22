b4aa7a52a1d3cfd49c6ff7e6c8c9b544b863ecf2
Abdurahman <abdurvhman1@mail.ru>
2022-06-22 18:59:20 +0300

    Функции

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
-document.body.innerHTML = '<h1>Hello from JavaScript<h1>';
+'use strict';
 
-console.log(2);
+// const func = (arr) => {
+
+// let sum = 0;
+//    for(let i = 0; i < arr.length; i++) {
+//       sum += arr[i];
+//    }
+
+//    return sum;
+// };
+
+// const array = [1, 2, 4, 55, 7, 4];
+
+// console.log(func(array));
+
+// const func = (array) => {
+//    const newArr = [];
+//    for(let i = 0; i < array.length; i++) {
+//      if (arr[i].length > 3) {
+//         newArr.push(arr[i]);
+//      }
+//    }
+//    return newArr;
+// }
+
+// const arr = ['aa', 'ffff', 'bbbfg'];
+
+// console.log(func(arr));
