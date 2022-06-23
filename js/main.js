23debe48133df0a71af01a00760b5315903bc4ae
Abdurahman <abdurvhman1@mail.ru>
2022-06-23 12:26:46 +0300

    Стрелочные функции

--- js/main.js
+++ js/main.js
@@ -1,29 +1,106 @@
 'use strict';
 
-// const func = (arr) => {
-
-// let sum = 0;
-//    for(let i = 0; i < arr.length; i++) {
-//       sum += arr[i];
-//    }
-
-//    return sum;
-// };
-
-// const array = [1, 2, 4, 55, 7, 4];
-
-// console.log(func(array));
+// С четными числами
 
 // const func = (array) => {
 //    const newArr = [];
 //    for(let i = 0; i < array.length; i++) {
-//      if (arr[i].length > 3) {
-//         newArr.push(arr[i]);
-//      }
+//       if (array[i] % 2 === 0) {
+//          newArr.push(array[i]);
+//       }
+//    }
+//       return newArr;
+// }
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+
+// console.log(func(arr));
+
+// С нечетными числами 
+
+// const func = (array) => {
+//    const newArr = [];
+
+//    for(let i = 0; i < array.length; i++) {
+//       if (array[i] % 2 !== 0) {
+//          newArr.push(arr[i]);
+//       }
 //    }
 //    return newArr;
 // }
 
-// const arr = ['aa', 'ffff', 'bbbfg'];
+// const arr = [1, 2, 3, 4, 5, 6, 7];
 
 // console.log(func(arr));
+
+// С положительными числами 
+
+// const func = (array) => {
+//    const newArr = [];
+
+//    for(let i = 0; i < array.length; i++) {
+//       if (array[i] > 0) {
+//          newArr.push(array[i])
+//       }
+//    }
+//    return newArr;
+// }
+
+// const arr = [1, -1, -4, 5];
+
+// console.log(func(arr));
+
+
+// С отрицательными числами
+
+// const func = (array) => {
+//    const newArr = [];
+
+//    for(let i = 0; i < array.length; i++) {
+//       if (array[i] < 0) {
+//          newArr.push(array[i])
+//       }
+//    }
+//    return newArr;
+// }
+
+// const arr = [1, -2, -4, 5, -10];
+
+// console.log(func(arr));
+
+
+// Со словами, длина которых больше 5 букв
+
+// const func = (array) => {
+//    const newArr = [];
+
+//    for(let i = 0; i < arr.length; i++) {
+//       if (array[i].length > 5) {
+//          newArr.push(array[i]);
+//       }
+//    }
+//    return newArr;
+// }
+
+// const arr = ['aaaaaaaa', 'bbb', 'gggggggg'];
+
+// console.log(func(arr));
+
+
+
+// Со словами, длина которых равняется 5 буквам
+
+const func = (array) => {
+   const newArr = [];
+
+   for(let i = 0; i < array.length; i++) {
+      if (array[i].length == 5) {
+         newArr.push(array[i]);
+      }
+   }
+   return newArr;
+}
+
+const arr = ['aaaaa', 'fff', 'eeeee', 'hhhh'];
+
+console.log(func(arr));
