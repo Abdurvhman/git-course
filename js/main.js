12e6cd90da9503c169f82d1232c25171c10549ce
Abdurahman <abdurvhman1@mail.ru>
2022-06-27 18:39:09 +0300

    fake exam

--- js/main.js
+++ js/main.js
@@ -1,78 +1,43 @@
 'use strict';
 
-// const  func = (a, b) => {
-//    return a ** b;
+// const func = (word) => {
+
+//    let str = '';
+
+//    for(let i = word.length -1; i > 0; i--) {
+//       str += word[i]; 
+//    }
+
+//    return str;
 // }
 
-// console.log(func(2, 3));
+// func('Hello');
 
-// const arr = ['Grozny','Moscow','NewYork','Germunchuk'];
+// const func = (number) => {
+//    if (number > 0) {
+//       return `-${number}`;
+//    }
+// }
+
+// func(3);
+
+// const arr = ['askhab', 'lg', 'lg', 'abdurah'];
 
 // const newArr = [];
 
+
 // for(let i = 0; i < arr.length; i++) {
-//    if (arr[i].length % 2 === 0) {
+//    if (arr[i] !== 'lg') {
 //       newArr.push(arr[i]);
 //    }
 // }
 
 // console.log(newArr);
 
-// const arr = [1, 21, 3, 4, 5];
 
-// let result = 0;
 
-// for(let i = 0; i < arr.length; i++) {
-//    if (arr[i] > result) {
-//       result = arr[i];
-//    }
+// const func = (lastname, name, otch) => {
+//    return `${lastname} ${name} ${otch[0]}.`;
 // }
 
-// console.log(result);
-
-
-// const func = (array, number) => {
-//    for(let i = 0; i < array.length; i++) {
-//       if (array[i] == number) {
-//          return true;
-//       }
-//    }
-// } 
-
-// const arr = [1, 2, 3, 4, 5];
-
-// console.log(func(arr, 6));
- 
-// const func = (array) => {
-//    for(let i = 0; i < array.length; i++) {
-//       if (array[i] > 0 && array[i] % 2 === 0) {
-//           newArr.push(array[i]);
-//       }
-//    }
-
-//    return newArr;
-// }
-
-// const arr = [-1, 2, -3, 4, -5, 6];
-
-// const newArr = [];
-
-// console.log(func(arr));
-
-// Напишите функцию, которая принимает два аргумента - массив слов и число, а возвращает новый массив со словами, длина которых равна второму второму аргументу
-//   ["Олень", "Сосна", "Грибы", "Береза", "Медведь"], 6 -> ['Береза]
-
-// const arr =  ["Олень", "Сосна", "Грибы", "Береза", "Медведь"];
-
-// const newArr = [];
-
-// const func = (array, number) => {
-//    for(let i = 0; i < array.length; i++) {
-//       if (array[i].length === number) {
-//          newArr.push(array[i]);     
-//       }
-//    }
-//    return newArr;
-// }
-
-// console.log(func(arr, 5));
+// console.log(func('Edilbiev', 'Abdurakhman', 'Abumuslimovich'));
